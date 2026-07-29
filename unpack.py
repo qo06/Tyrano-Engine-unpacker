61cf8740a02793499337143265c7844bddc2bf1d
Rosa <108557334+qo06@users.noreply.github.com>
2026-07-29 09:59:07 +0400

    Update unpack.py

--- unpack.py
+++ unpack.py
@@ -28,30 +28,33 @@ def extract(File,OutDir=""):
 
     ExecPath = File.split(".")[0] + sep
     
-    with open(File,'rb') as fr:
+    fr = open(File,'rb')
+    
+    #with open(File,'rb') as fr:
 
-        _,val_2,_,IndexSize = unpack('<IIII',fr.read(16))
+    _,val_2,_,IndexSize = unpack('<IIII',fr.read(16))
         
-        fr.seek(16,0)
+    fr.seek(16,0)
         
-        Index =  loads( fr.read(IndexSize).decode('utf8') )
+    Index =  loads( fr.read(IndexSize).decode('utf8') )
 
-        Index = FolderLoop(Index,\
-                           Current='',\
-                           sf=val_2+8)
+    Index = FolderLoop(Index,\
+                       Current='',\
+                       sf=val_2+8)
         
-        for i in Index:
-            print(i[0])
-            Path = ExecPath + i[0]
-            if i[1] == 0 and i[2] == 0:
-                makedirs(Path,exist_ok=True)
-            else:
-                fr.seek(i[2],0)
-                Data = fr.read(i[1])
-                fw = open(Path,'wb')
-                fw.write(Data)
-                fw.close()
-        fr.close()
+    for i in Index:
+        print(i[0])
+        Path = ExecPath + i[0]
+        if i[1] == 0 and i[2] == 0:
+            makedirs(Path,exist_ok=True)
+        else:
+            fr.seek(i[2],0)
+            Data = fr.read(i[1])
+            
+            fw = open(Path,'wb')
+            fw.write(Data)
+            fw.close()
+    fr.close()
 
 if __name__ == '__main__':
     extract(sys.argv[1])
